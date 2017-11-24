b118fbbf4413fc81913a3429aa30ecf5bde94d40
Moemen <moemen.a1@gmail.com>
2017-11-25 00:39:41 +0400

    fixing initial unit tests

--- src/app/redirect/redirect.component.spec.ts
+++ src/app/redirect/redirect.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RedirectComponent } from './redirect.component';
+import {UrlShortenerService} from '../url-shortener.service';
+import {StorageService} from '../storage.service';
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 describe('RedirectComponent', () => {
   let component: RedirectComponent;
@@ -8,7 +13,12 @@ describe('RedirectComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RedirectComponent ]
+      declarations: [ RedirectComponent ],
+      providers: [
+        UrlShortenerService,
+        StorageService,
+        { provide: ActivatedRoute,  useValue: {params: Observable.of({hash: 123})} }
+      ]
     })
     .compileComponents();
   }));
