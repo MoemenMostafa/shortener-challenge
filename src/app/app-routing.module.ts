8ce34adaf4c9b052a4b353e06e2f8e7a3743d576
Moemen <moemen.a1@gmail.com>
2017-11-24 09:23:06 +0400

    removing references to deleted component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ShortUrlComponent} from './short-url/short-url.component';
-import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {RedirectComponent} from './redirect/redirect.component';
 
